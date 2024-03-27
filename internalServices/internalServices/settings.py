7f808813e1f04fd347ee1a32f7d4e13fc73108c3
sayed azp <s.azp@idec.com.sa>
2024-03-27 11:06:51 +0200

    adding null and blank contrains to quote-comps

--- internalServices/internalServices/settings.py
+++ internalServices/internalServices/settings.py
@@ -89,8 +89,6 @@ DATABASES = {
         # 'PASSWORD': 'your_db_password',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
     }
 }
 
